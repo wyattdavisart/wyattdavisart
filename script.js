717a00d098bed3e839c109250f178159af7dd4b0
wyattdavisart <wyatt.c.davis@hotmail.com>
2026-03-20 09:55:31 -0700

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,17 @@
+document.addEventListener("DOMContentLoaded", () => {
+  const sections = document.querySelectorAll('.text');
 
+  const observer = new IntersectionObserver((entries) => {
+    entries.forEach(entry => {
+      if (entry.isIntersecting) {
+        entry.target.classList.add('show');
+      }
+    });
+  }, {
+    threshold: 0.2
+  });
+
+  sections.forEach(section => {
+    observer.observe(section);
+  });
+});
